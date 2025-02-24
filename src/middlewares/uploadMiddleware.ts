ec2d03ee1deff0435214b606b5b98f719bb0e597
Muhammed Murad <129909845+mpro775@users.noreply.github.com>
2025-02-24 03:18:51 +0300

    Update uploadMiddleware.ts

--- src/middlewares/uploadMiddleware.ts
+++ src/middlewares/uploadMiddleware.ts
@@ -1,19 +1,11 @@
+// src/middlewares/uploadMiddleware.ts
 import multer from "multer";
-import path from "path";
 import { v4 as uuidv4 } from "uuid";
+import path from "path";
 
-// إعداد تخزين الملفات
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/"); // المجلد الذي سيتم تخزين الصور فيه
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = `${uuidv4()}${path.extname(file.originalname)}`;
-    cb(null, uniqueName); // اسم الملف الفريد
-  },
-});
+// استخدم memoryStorage بدل diskStorage
+const storage = multer.memoryStorage();
 
-// التحقق من نوع الملف
 const fileFilter = (req: any, file: any, cb: any) => {
   if (file.mimetype.startsWith("image")) {
     cb(null, true);
@@ -22,5 +14,4 @@ const fileFilter = (req: any, file: any, cb: any) => {
   }
 };
 
-// تصدير Middleware
 export const upload = multer({ storage, fileFilter });
